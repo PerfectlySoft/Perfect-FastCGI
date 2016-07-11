618d012b736a5d0a295c7f9de420853ec170957d
Kyle Jessup <badthing@gmail.com>
2016-07-11 15:21:50 -0400

    Removed bad test

--- Tests/PerfectFastCGI/PerfectFastCGITests.swift
+++ Tests/PerfectFastCGI/PerfectFastCGITests.swift
@@ -3,11 +3,8 @@ import XCTest
 
 class PerfectFastCGITests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(PerfectFastCGI().text, "Hello, World!")
+		
     }
-
 
     static var allTests : [(String, (PerfectFastCGITests) -> () throws -> Void)] {
         return [
